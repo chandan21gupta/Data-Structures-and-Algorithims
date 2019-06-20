f1b9ff0ba1fc3e8df5f9eebade4fbc4f64dde1d5
kashinkoji <chandan18386@iiitd.ac.in>
2019-06-20 14:44:58 +0530

    Updated - Graph Representation

--- Graph.java
+++ Graph.java
@@ -4,43 +4,68 @@ import java.util.*;
 class Graph {
 
   int V;
-  LinkedList<Integer> arr[];
+  Node[] arr;
 
-
-  Graph(int V) {
+  Graph(int V){
     this.V = V;
-    arr = new LinkedList[V];
+    arr = new Node[this.V];
 
-    for(int i=0;i<V;i++){
-      arr[i] = new LinkedList<>();
+    for(int i=0;i<this.V;i++){
+      arr[i] = new Node(i);
     }
   }
 
+
   public void edgeMaker(int src,int dest){
-    arr[src].add(dest);
-    //arr[dest].add(src); For undirected graphs
+    Node n = arr[src];
+    while(n.next!=null){
+      n=n.next;
+    }
+    n.next = new Node(dest);
   }
 
   public void printGraph(){
-    for(int i=0;i<V;i++){
-      System.out.print(i+" ");
-      for(Integer Node : arr[i]){
-        System.out.print(Node+" ");
+    for(int i=0;i<this.V;i++){
+      System.out.print(arr[i].data+" ");
+      Node n = arr[i].next;
+      while(n!=null){
+        System.out.print(n.data+" ");
+        n=n.next;
       }
       System.out.println();
     }
   }
 
+  class Node{
+    int data;
+    int color;
+    Node next;
+    Node(int data){
+      this.data = data;
+      color = 0;
+    }
+  }
+
+
+
+
   public static void main(String[] args){
     int V = 5;
     Graph graph = new Graph(V);
     graph.edgeMaker(0,1);
+    graph.edgeMaker(1,0);
     graph.edgeMaker(0,4);
+    graph.edgeMaker(4,0);
     graph.edgeMaker(1,2);
+    graph.edgeMaker(2,1);
     graph.edgeMaker(1,3);
+    graph.edgeMaker(3,1);
     graph.edgeMaker(1,4);
+    graph.edgeMaker(4,1);
     graph.edgeMaker(2,3);
+    graph.edgeMaker(3,2);
     graph.edgeMaker(3,4);
+    graph.edgeMaker(4,3);
     graph.printGraph();
   }
 }
