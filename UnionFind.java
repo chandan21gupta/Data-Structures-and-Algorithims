e8a0e669b05d8634cb9fbd8b02e3d3268d924378
kashinkoji <chandan18386@iiitd.ac.in>
2019-07-26 02:56:26 +0530

    QuickUnion Restored

--- UnionFind.java
+++ UnionFind.java
@@ -1,55 +1,55 @@
 import java.io.*;
-// class QuickFind {
-// 	int n;
-// 	int[] sets;
+class QuickFind {
+	int n;
+	int[] sets;
 
-// 	public QuickFind(int n) {
-// 		this.n = n;
-// 		sets = new int[n];
-// 		for(int i=0;i<n;i++){
-// 			sets[i] = i;
-// 		}
-// 	}
+	public QuickFind(int n) {
+		this.n = n;
+		sets = new int[n];
+		for(int i=0;i<n;i++){
+			sets[i] = i;
+		}
+	}
 
-// 	public boolean slowconnected(int p, int q){
-// 		return sets[p] == sets[q];
-// 	}
+	public boolean slowconnected(int p, int q){
+		return sets[p] == sets[q];
+	}
 
-// 	public int quickfind(int p){
-// 		return sets[p];
-// 	}
+	public int quickfind(int p){
+		return sets[p];
+	}
 
-// 	public void slowunion(int p,int q) {
-// 		if(slowconnected(p,q)){
-// 			return;
-// 		}
+	public void slowunion(int p,int q) {
+		if(slowconnected(p,q)){
+			return;
+		}
 
-// 		int pid = quickfind(p);
-// 		int qid = quickfind(q);
+		int pid = quickfind(p);
+		int qid = quickfind(q);
 
-// 		for(int i=0;i<n;i++){
-// 			if(sets[i] == pid){
-// 				sets[i] = qid;
-// 			}
-// 		}
-// 	}
+		for(int i=0;i<n;i++){
+			if(sets[i] == pid){
+				sets[i] = qid;
+			}
+		}
+	}
 
-// 	public void print_set(){
-// 		for(int i=0;i<this.n;i++){
-// 			System.out.print(sets[i]+" ");
-// 		}
-// 	}
+	public void print_set(){
+		for(int i=0;i<this.n;i++){
+			System.out.print(sets[i]+" ");
+		}
+	}
 
-// }
+}
 
 
 
-class UnionFind {
+class QuickUnion {
 	
 	int[] id;
 	int[] size;
 
-	public UnionFind(int n){
+	public QuickUnion(int n){
 		id = new int[n+1];
 		size = new int[n+1];
 		for(int i=1;i<n+1;i++){
@@ -96,28 +96,12 @@ class UnionFind {
 	}
 
 
+}
 
+class UnionFind{
 
 	public static void main(String[] args) throws java.io.IOException {
 
-		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(buffer.readLine());
-		UnionFind q = new UnionFind(n);
-		int q1 = Integer.parseInt(buffer.readLine());
-		for(int i=0;i<q1;i++){
-			String s1 = buffer.readLine();
-			String[] s = s1.split(" ");
-			if(s[0].equals("1")){
-				q.quickunion(Integer.parseInt(s[1]),Integer.parseInt(s[2]));
-			}
-			else{
-				if(q.quickconnected(Integer.parseInt(s[1]),Integer.parseInt(s[2]))){
-					System.out.println("YES");
-				}
-				else{
-					System.out.println("NO");
-				}
-			}
-		}
+		
 	}
 }
