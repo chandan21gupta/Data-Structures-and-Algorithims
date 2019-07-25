7fdbaa0cca04f3e9fdb3544567682822a2b5d007
kashinkoji <chandan18386@iiitd.ac.in>
2019-07-26 02:38:24 +0530

    dijkstra's shortest path

--- graphs/weightedgraph.java
+++ graphs/weightedgraph.java
@@ -69,21 +69,70 @@ class Graph {
         
         return distances;
     }
+
+    int[] dijkstra(int s) {
+        int[] distances = new int[V];
+        int[] visited = new int[V];
+        for(int i=0;i<V;i++) {
+            if(i != s) {
+                distances[i] = Integer.MAX_VALUE-1000;
+            }
+        }
+        int min_index = s;
+        while(min_index != -1){
+            visited[min_index] = 1;
+            for(Edge e : arr[min_index]) {
+                int d = distances[min_index] + e.weight;
+                if(d<distances[e.dest]) {
+                    distances[e.dest] = d;
+                } 
+            }
+            min_index = minimim_weight(visited,distances);
+        }
+        return distances;
+    }
+
+
+    int minimim_weight(int[] visited,int[] distances) {
+        int min_index = -1;
+        int[] temp = new int[distances.length];
+        for(int i=0;i<distances.length;i++) {
+            temp[i] = distances[i];
+        }
+        for(int i=0;i<distances.length;i++) {
+            if(visited[i] != 1) {
+                for(int j=0;j<distances.length;j++) {
+                    if(distances[j] == temp[i]) {
+                        return j;
+                    }
+                }
+            }
+        }
+        return min_index;
+    }
+
+
+
 }
 class weightedgraph {
     public static void main(String[] args) {
-        Graph g = new Graph(6);
-        g.addedge(0, 1, 10);
+        Graph g = new Graph(5);
+        g.addedge(0, 1, 3);
+        g.addedge(1, 0, 3);
+        g.addedge(0, 3, 1);
+        g.addedge(3, 0, 1);
+        g.addedge(1, 2, 1);
         g.addedge(2, 1, 1);
-        g.addedge(3, 2, -2);
-        g.addedge(4, 3, -1);
-        g.addedge(5, 4, 1);
-        g.addedge(0, 5, 8);
-        g.addedge(1, 3, 2);
-        g.addedge(4, 1, -4);
-
+        g.addedge(1, 3, 7);
+        g.addedge(3, 1, 7);
+        g.addedge(1, 4, 5);
+        g.addedge(4, 1, 5);
+        g.addedge(2, 4, 7);
+        g.addedge(4, 2, 7);
+        g.addedge(3, 4, 2);
+        g.addedge(4, 3, 2);
         //g.display();
-        int[] arr = g.bellman_ford();
+        int[] arr = g.dijkstra(3);
         for(int i=0;i<arr.length;i++) {
             System.out.print(arr[i]+" ");
         }
